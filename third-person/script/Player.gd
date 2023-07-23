e5120273a046316d8b0a77c08bd7b278061ea00b
Rıza Kaan Uçak <rzakaan@gmail.com>
2023-07-23 21:24:42 +0300

    third-person animation added

--- third-person/script/Player.gd
+++ third-person/script/Player.gd
@@ -1,12 +1,23 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var SPEED = 3.0
+var JUMP_VELOCITY = 4.5
+const walking_speed = 3.0
+const running_speed = 8.0
+var is_running = false
+var is_locked = false
 
-const sense_horizontal = 0.5
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# export settings
+@export var sense_horizontal = 0.5
+@export var sense_vertical = 0.5
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# game components
+@onready var animation_player = $Visuals/MixamoBase/AnimationPlayer
+@onready var camera_mount = $CameraMount
+@onready var visuals = $Visuals
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -14,26 +25,61 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sense_horizontal))
-	
+		visuals.rotate_y(deg_to_rad(event.relative.x * sense_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sense_vertical))
+
 func _physics_process(delta):
-	# Add the gravity.
-	# After change gravity settings
+	if not animation_player.is_playing():
+		is_locked = false
+	
+	if Input.is_action_pressed("kick"):
+		if animation_player.current_animation != "kick":
+			animation_player.play("kick")
+			is_locked = true
+	
+	if Input.is_action_pressed("jump"):
+		is_locked = true
+			
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		is_running = true
+	else:
+		SPEED = walking_speed
+		is_running = false
+	
+	# gravity settings
 	#if not is_on_floor():
 		#velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	
+	# handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		# there is a direction and speed so we are walking or running
+		
+		if not is_locked:
+			if is_running:
+				if animation_player.current_animation != "running":
+					animation_player.play("running")
+			else:
+				if animation_player.current_animation != "walking":
+					animation_player.play("walking")	
+			
+			visuals.look_at(position + direction)
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if not is_locked:
+			if animation_player.current_animation != "idle":
+				animation_player.play("idle")
+			
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	
+	if not is_locked:
+		move_and_slide()
